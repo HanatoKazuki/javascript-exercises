79668a2b8d8e5fd150c37d67c8da0a544886ed2c
Kazuki <136700294+HanatoKazuki@users.noreply.github.com>
2026-08-13 21:31:13 +0800

    Create exercise1.js

---
+++ exercise1.js
@@ -0,0 +1,6 @@
+fullName = "Lee Zhi Han"
+age = 17
+isStudent = true
+greeting = "Hello, my name is "+fullName + ", I am "+age + " years old, and it is "+isStudent + " that I am a student."
+
+console.log(greeting)
